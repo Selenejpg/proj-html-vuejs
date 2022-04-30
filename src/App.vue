77c2d64c2cbd19b9d5d411f7439a6aeca8418691
noemipintaldi <noemipintaldi@gmail.com>
2022-04-30 21:40:15 +0200

    header completato

--- src/App.vue
+++ src/App.vue
@@ -27,11 +27,13 @@ export default {
 
 @import "bootstrap/dist/css/bootstrap.min.css";
 @import "./components/style/variabili.scss";
+@import url('https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@300&family=Montserrat:wght@100;200;300;500&family=Mulish&family=Nunito:wght@200&family=Poppins:wght@200&family=Prompt&family=Ubuntu:wght@300&family=Varela+Round&display=swap');
 
 *{
   margin: 0;
   padding: 0;
   box-sizing: border-box;
+  color: white;
 }
 
 </style>
--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -1,7 +1,7 @@
 <template>
   <header>
     <!-- Header logo e menu -->
-    <div class="p-4 mt-2 d-flex justify-content-between align-items-center">
+    <div class="p-4 d-flex justify-content-between align-items-center">
       <!-- logo -->
       <div>
         <img class="logo-ayada ms-5" src="../assets/img/gym_logo_1x.png" alt="">
@@ -33,18 +33,17 @@
     </div>
     
     <!-- titolo -->
-    <div>
-      <h1></h1>
+    <div class="d-flex m-top text-white justify-content-center align-items-center">
+      <h1 class="title">NO PAIN NO GAIN</h1>
     </div>
 
     <!-- paragrafo sottostante e bottone -->
-    <div>
-      <div>
-        <p></p>
-        <button></button>
-      </div>
+    <div class="d-flex mt-2 justify-content-center">
+      <p>Motivation is what gets you started. Habit is what keeps you going.</p>
     </div>
-    
+    <div class="d-flex mt-3 justify-content-center">
+      <button type="button" class="btn btn-primary">JOIN US TODAY</button>
+    </div>
   </header>
 </template>
 
@@ -59,10 +58,10 @@ export default {
 @import "./style/variabili.scss";
 
   header{
-    height: 500px;
     background-image: url(../assets/img/home-banner.jpg);
     background-repeat: no-repeat;
     background-size: cover;
+    height: 750px;
   }
 
   .logo-ayada{
@@ -82,5 +81,26 @@ export default {
 
   a:hover{
     color: $chestnutrose;
+  }
+
+  .m-top{
+    margin-top: 13%;
+  }
+
+  .title {
+    font-weight: 700;
+    font-size: 70px;
+  }
+
+  button{
+    background-color: transparent;
+    border-color: lightgray;
+    color: lightgray;
+    padding: 15px;
+  }
+
+  button:hover {
+    background-color: transparent;
+    border-color: $chestnutrose;
   }
 </style>
