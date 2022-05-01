5083b50a57c6de360163242d4bfcbd7b7aa3c5dd
noemipintaldi <noemipintaldi@gmail.com>
2022-05-01 11:53:49 +0200

    Seconda sezione completata

--- src/components/partials/SecondaSezione.vue
+++ src/components/partials/SecondaSezione.vue
@@ -3,22 +3,22 @@
   <div class="d-flex justify-content-center">
       <img class="margin-polygon" src="../../assets/img/Polygon 1.png" alt="">
   </div>
-  <div class="d-flex justify-content-center">
+  <div class="d-flex padding justify-content-evenly marginphoto">
     <div class="">
       <img src="../../assets/img/home_img-compressor-400x222.jpg" alt="">
     </div>
-    <div class="margin-left">
-      <h2 class="">Sign Up for a free training session!</h2>
-      <span class="gray">Name*</span>
-      <div class="input-group mb-3">
+    <div class="margin-right">
+      <h2 class="mb-4">Sign Up for a free training session!</h2>
+      <span class="gray padding-input">Name*</span>
+      <div class="input-group mb-4">
         <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
       </div>
-      <span class="gray">Email</span>
-      <div class="input-group mb-3">
+      <span class="gray padding-input">Email</span>
+      <div class="input-group mb-5">
         <span class="input-group-text" id="basic-addon1">@</span>
         <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
       </div>
-      <button type="button" class="btn btn-danger">Danger</button>
+      <button type="button" class="btn w-custom btn-danger">START NOW</button>
     </div>
   </div>
 </div>
@@ -33,6 +33,8 @@ export default {
 </script>
 
 <style scoped lang="scss">
+@import "../style/variabili.scss";
+
   .margin-polygon{
     margin-top: -2px
   }
@@ -43,5 +45,29 @@ export default {
 
   .bg-color{
     background-color: #202123;
+  }
+
+  .marginphoto{
+    margin-left: 240px;
+  }
+
+  .margin-right{
+    margin-right: 160px;
+  }
+
+  .padding {
+    padding-top: 90px;
+    padding-bottom: 110px;
+  }
+
+  .w-custom {
+    width: 100%;
+    background-color: $chestnutrose;
+    border-color: $chestnutrose;
+  }
+
+  .btn:hover{
+    background-color: $chestrosevibrant;
+    border-color: $chestrosevibrant;
   }
 </style>
