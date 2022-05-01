ea4a796c1a6769283482a0a22dcaf0ee63efd9a1
noemipintaldi <noemipintaldi@gmail.com>
2022-05-01 18:05:18 +0200

    quinta sezione completata

--- src/components/partials/QuintaSezione.vue
+++ src/components/partials/QuintaSezione.vue
@@ -1,7 +1,14 @@
 <template>
   <div>
-   
+    <div class="bg-hero text-center d-flex justfy-content-center align-item-center align-items-center">
+      <div class="margin">
+        <p class="mb-4">"I fear not the man who has practiced 10.000 kicks once, but i fear the man who has practiced one kick 10.000 times"</p>
+        <span>BRUCE LEE · QUOTE OF THE DAY </span>
+      </div>
+      
+    </div>
   </div>
+  
 </template>
 
 <script>
@@ -13,5 +20,20 @@ export default {
 
 <style scoped lang="scss">
 @import "../style/variabili.scss";
- /*Inserire style componente*/
+
+.bg-hero{
+  background-image: url(../../assets/img/home-testimonial-bg.jpg);
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+  height: 430px;
+}
+
+.margin{
+  margin-left: 30%;
+  margin-right: 30%;
+  font-size: 20px;
+  font-weight: 700;
+}
+
 </style>
